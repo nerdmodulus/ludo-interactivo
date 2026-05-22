e369013a46ce0cc8c43b84b012c88b509f7fbd68
nerdmodulus <124804213+nerdmodulus@users.noreply.github.com>
2026-05-22 13:59:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database().ref("ludoTrivia");
 
-const EXCEL_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSaJaro7CAEcOrUyYxHjAs0cdwbJAWGic8BoDgisqs6ssxlEzWb2XArVsKiCfAJ0llAn6ncEnZqWIjy/pub?output=csv";
+const EXCEL_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSaJaro7CAEcOrUyYxHjAs0cdwbJAWGic8BoDgisqs6ssxlEzWb2XArVsKiCfAJ0llAn6ncEnZqWIjy/pub?gid=0&single=true&output=csv";
 
 let bancoPreguntas = [];
 let miRol = "";
